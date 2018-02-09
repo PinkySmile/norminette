f568315f89caafc55441e81f2dbdd0872b6898d1
Gegel85 <Gegel85@laposte.net>
2018-02-09 18:21:15 +0100

    Added few tests

---
+++ tests/F4.c
@@ -0,0 +1,138 @@
+/*
+** EPITECH PROJECT, 2018
+** F4
+** File description:
+** F4
+*/
+
+void	this_function_is_exactly_20_lines(void)
+{
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+}
+
+void	this_function_is_exactly_21_lines(void)
+{
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+}
+
+void	this_function_shouldn_t_be_detected(void)
+{
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+}
+
+void	this_function_shouldn_t_be_detected2(void)
+{
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	
+	test;
+
+
+	test;
+					
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+}
+
+void	this_function_should_have_21_lines(void)
+{
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+	test;
+
+	
+	test;
+
+
+
+	test;
+	test;
+	test;
+	test;
+	test;
+
+
+	
+	test;
+	test;
+	test;
+	test;
+	test;
+	
+	test;
+	test;
+}
