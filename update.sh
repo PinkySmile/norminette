203e07b890dab4041a97664d5521eeb2d5b09486
Andgel <andgel.halley@epitech.eu>
2018-05-27 16:16:40 +0200

    Verify if compile failed

--- update.sh
+++ update.sh
@@ -2,9 +2,12 @@
 cd $HOME/norminette
 if [ $(($(git pull | wc -l) * $([ $1t == "--forcet" ]; echo -n $?))) != 1 ]
 then
-    make re
-    echo "coping nominette to /bin/norminette"
-    sudo cp $HOME/norminette/norminette /bin/
-    echo "adding manpage"
-    sudo cp $HOME/norminette/manpage.1.gz /usr/share/man/man1/norminette.1.gz
+    if make re; then
+	echo "coping nominette to /bin/norminette"
+	sudo cp $HOME/norminette/norminette /bin/
+	echo "adding manpage"
+	sudo cp $HOME/norminette/manpage.1.gz /usr/share/man/man1/norminette.1.gz
+    else
+	echo "Error: Compilation failed"
+    fi
 fi
